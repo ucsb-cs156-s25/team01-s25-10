5809fa8b4a794660c35adadd8c482fc9fc5d8c46
Junjie Liu <junjieliu@ucsb.edu>
2025-04-25 03:45:23 -0700

    added post and get endpoints for articles

---
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -0,0 +1,100 @@
+package edu.ucsb.cs156.example.controllers;
+
+import edu.ucsb.cs156.example.entities.UCSBArticles;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
+import edu.ucsb.cs156.example.repositories.UCSBArticlesRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+import java.time.LocalDateTime;
+
+/**
+ * This is a REST controller for UCSBDates
+ */
+
+@Tag(name = "UCSBArticles")
+@RequestMapping("/api/ucsbarticles")
+@RestController
+@Slf4j
+public class ArticlesController extends ApiController {
+
+    @Autowired
+    UCSBArticlesRepository ucsbArticlesRepository;
+
+    /**
+     * List all UCSB articles
+     * 
+     * @return an iterable of UCSBArticles
+     */
+    @Operation(summary= "List all ucsb articles")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/all")
+    public Iterable<UCSBArticles> allUCSBArticles() {
+        Iterable<UCSBArticles> articles = ucsbArticlesRepository.findAll();
+        return articles;
+    }
+
+    /**
+     * Get a single article by id
+     * 
+     * @param id the id of the article
+     * @return a UCSBArticle
+     */
+    @Operation(summary= "Get a single article")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public UCSBArticles getById(
+            @Parameter(name="id") @RequestParam Long id) {
+        UCSBArticles ucsbArticle = ucsbArticlesRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(UCSBArticles.class, id));
+
+        return ucsbArticle;
+    }
+
+    /**
+     
+     */
+    @Operation(summary= "Create a new article")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/post")
+    public UCSBArticles postUCSBArticles(
+            @Parameter(name="title") @RequestParam String title,
+            @Parameter(name="url") @RequestParam String url,
+            @Parameter(name="explanation") @RequestParam String explanation,
+            @Parameter(name="email") @RequestParam String email,
+            @Parameter(name="localDateTime", description="date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("localDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime localDateTime)
+            throws JsonProcessingException {
+
+        log.info("localDateTime={}", localDateTime);
+
+        UCSBArticles ucsbArticle = new UCSBArticles();
+        ucsbArticle.setTitle(title);
+        ucsbArticle.setUrl(url);
+        ucsbArticle.setExplanation(explanation);
+        ucsbArticle.setEmail(email);
+        ucsbArticle.setLocalDateTime(localDateTime);
+
+        UCSBArticles savedUcsbArticle = ucsbArticlesRepository.save(ucsbArticle);
+
+        return savedUcsbArticle;
+    }
+}
---
+++ src/test/java/edu/ucsb/cs156/example/controllers/ArticlesControllerTests.java
@@ -0,0 +1,204 @@
+package edu.ucsb.cs156.example.controllers;
+
+import edu.ucsb.cs156.example.repositories.UserRepository;
+import edu.ucsb.cs156.example.testconfig.TestConfig;
+import edu.ucsb.cs156.example.ControllerTestCase;
+import edu.ucsb.cs156.example.entities.UCSBArticles;
+import edu.ucsb.cs156.example.repositories.UCSBArticlesRepository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+import java.time.LocalDateTime;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(controllers = ArticlesController.class)
+@Import(TestConfig.class)
+public class ArticlesControllerTests extends ControllerTestCase {
+
+        @MockBean
+        UCSBArticlesRepository ucsbArticlesRepository;
+
+        @MockBean
+        UserRepository userRepository;
+
+        // Authorization tests for /api/ucsbArticles/admin/all
+
+        @Test
+        public void logged_out_users_cannot_get_all() throws Exception {
+                mockMvc.perform(get("/api/ucsbarticles/all"))
+                                .andExpect(status().is(403)); // logged out users can't get all
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_users_can_get_all() throws Exception {
+                mockMvc.perform(get("/api/ucsbarticles/all"))
+                                .andExpect(status().is(200)); // logged
+        }
+
+        @Test
+        public void logged_out_users_cannot_get_by_id() throws Exception {
+                mockMvc.perform(get("/api/ucsbarticles?id=7"))
+                                .andExpect(status().is(403)); // logged out users can't get by id
+        }
+
+        // Authorization tests for /api/ucsbArticles/post
+        // (Perhaps should also have these for put and delete)
+
+        @Test
+        public void logged_out_users_cannot_post() throws Exception {
+                mockMvc.perform(post("/api/ucsbarticles/post"))
+                                .andExpect(status().is(403));
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_regular_users_cannot_post() throws Exception {
+                mockMvc.perform(post("/api/ucsbarticles/post"))
+                                .andExpect(status().is(403)); // only admins can post
+        }
+
+        // // Tests with mocks for database actions
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void test_that_logged_in_user_can_get_by_id_when_the_id_exists() throws Exception {
+
+                // arrange
+                LocalDateTime ldt = LocalDateTime.parse("2022-04-20T00:00:00");
+
+                UCSBArticles ucsbArticle = UCSBArticles.builder()
+                                .title("\tUsing testing-playground with React Testing Library")
+                                .url("https://dev.to/katieraby/using-testing-playground-with-react-testing-library-26j7")
+                                .explanation("Helpful when we get to front end development")
+                                .email("phtcon@ucsb.edu\t")
+                                .localDateTime(ldt)
+                                .build();
+
+                when(ucsbArticlesRepository.findById(eq(1L))).thenReturn(Optional.of(ucsbArticle));
+
+                // act
+                MvcResult response = mockMvc.perform(get("/api/ucsbarticles?id=1"))
+                                .andExpect(status().isOk()).andReturn();
+
+                // assert
+
+                verify(ucsbArticlesRepository, times(1)).findById(eq(1L));
+                String expectedJson = mapper.writeValueAsString(ucsbArticle);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void test_that_logged_in_user_can_get_by_id_when_the_id_does_not_exist() throws Exception {
+
+                // arrange
+
+                when(ucsbArticlesRepository.findById(eq(7L))).thenReturn(Optional.empty());
+
+                // act
+                MvcResult response = mockMvc.perform(get("/api/ucsbarticles?id=7"))
+                                .andExpect(status().isNotFound()).andReturn();
+
+                // assert
+
+                verify(ucsbArticlesRepository, times(1)).findById(eq(7L));
+                Map<String, Object> json = responseToJson(response);
+                assertEquals("EntityNotFoundException", json.get("type"));
+                assertEquals("UCSBArticles with id 7 not found", json.get("message"));
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_user_can_get_all_ucsbArticles() throws Exception {
+
+                // arrange
+                LocalDateTime ldt1 = LocalDateTime.parse("2022-04-20T00:00:00");
+
+                UCSBArticles ucsbArticle1 = UCSBArticles.builder()
+                                .title("Using testing-playground with React Testing Library")
+                                .url("https://dev.to/katieraby/using-testing-playground-with-react-testing-library-26j7")
+                                .explanation("Helpful when we get to front end development")
+                                .email("phtcon@ucsb.edu\t")
+                                .localDateTime(ldt1)
+                                .build();
+
+                LocalDateTime ldt2 = LocalDateTime.parse("2022-04-19T00:00:00");
+
+                UCSBArticles ucsbArticle2 = UCSBArticles.builder()
+                                .title("Handy Spring Utility Classes")
+                                .url("https://twitter.com/maciejwalkowiak/status/1511736828369719300?t=gGXpmBH4y4eY9OBSUInZEg&s=09")
+                                .explanation("A lot of really useful classes are built into Spring")
+                                .email("phtcon@ucsb.edu\t")
+                                .localDateTime(ldt2)
+                                .build();
+
+                ArrayList<UCSBArticles> expectedArticles = new ArrayList<>();
+                expectedArticles.addAll(Arrays.asList(ucsbArticle1, ucsbArticle2));
+
+                when(ucsbArticlesRepository.findAll()).thenReturn(expectedArticles);
+
+                // act
+                MvcResult response = mockMvc.perform(get("/api/ucsbarticles/all"))
+                                .andExpect(status().isOk()).andReturn();
+
+                // assert
+
+                verify(ucsbArticlesRepository, times(1)).findAll();
+                String expectedJson = mapper.writeValueAsString(expectedArticles);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
+
+        @WithMockUser(roles = { "ADMIN", "USER" })
+        @Test
+        public void an_admin_user_can_post_a_new_ucsbarticle() throws Exception {
+                // arrange
+
+                LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
+
+                UCSBArticles ucsbArticle1 = UCSBArticles.builder()
+                                .title("a")
+                                .url("https://team01-dev-junjiel123.dokku-10.cs.ucsb.edu")
+                                .explanation("b")
+                                .email("phtcon@ucsb.edu")
+                                .localDateTime(ldt1)
+                                .build();
+
+                when(ucsbArticlesRepository.save(eq(ucsbArticle1))).thenReturn(ucsbArticle1);
+
+                // act
+                MvcResult response = mockMvc.perform(
+                                post("/api/ucsbarticles/post?title=a&url=https://team01-dev-junjiel123.dokku-10.cs.ucsb.edu&explanation=b&email=phtcon@ucsb.edu&localDateTime=2022-01-03T00:00:00")
+                                                .with(csrf()))
+                                .andExpect(status().isOk()).andReturn();
+
+                // assert
+                verify(ucsbArticlesRepository, times(1)).save(ucsbArticle1);
+                String expectedJson = mapper.writeValueAsString(ucsbArticle1);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
+}
